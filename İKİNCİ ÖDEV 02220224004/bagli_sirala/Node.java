83d41cd69746e4ee4e4e4e552db22e4cae76655f
RANANUROKTAY <136755355+RANANUROKTAY@users.noreply.github.com>
2024-04-03 17:21:59 +0300

    Add files via upload

---
+++ İKİNCİ ÖDEV 02220224004/bagli_sirala/Node.java
@@ -0,0 +1,14 @@
+package bagli_sirala31;
+/**
+ *
+ * @author RANA NUR OKTAY
+ */
+class Node {
+    int x;
+    int y;
+
+    Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
